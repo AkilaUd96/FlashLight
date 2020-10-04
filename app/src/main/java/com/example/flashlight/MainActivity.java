1fdb3a685271a044a929a5aea4afd7b88f980c72
Akila Udara <it17134118@my.sliit.lk>
2020-10-04 12:29:18 -0700

    Added dexter dependency

--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.flashlight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
