71cbf8498b261685e8614aac3b4a772737894ec3
Akila Udara <it17134118@my.sliit.lk>
2020-10-04 12:41:16 -0700

    Added dexter dependency

--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.flashlight;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 ImageButton imageButton;
@@ -12,6 +21,25 @@ ImageButton imageButton;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        imageButton = findViewById(R.id.toff);
 
+        Dexter.withContext(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                Toast.makeText(MainActivity.this, "Camera permission is required", Toast.LENGTH_SHORT);
+
+            }
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+            }
+        })
     }
 }
